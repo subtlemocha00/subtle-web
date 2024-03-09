4355b850dfe598110dbb2b4a6e56c751666a132e
subtlemocha00 <kturley@gmail.com>
2024-03-09 00:55:39 -0500

    color and style changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
-window.addEventListener("DOMContentLoaded", (e) => {
-	const scrollUpButton = document.getElementById("scrollUpButton");
+// window.addEventListener("DOMContentLoaded", (e) => {
+// 	const scrollUpButton = document.getElementById("scrollUpButton");
 
-	const handleScroll = () => {
-		if (!navContainer) return;
-		if (window.scrollY === 0) {
-			navContainer.classList.remove("navShrink");
-			navParent.classList.remove("navParentShrink");
-			// navLogo.classList.add("navbar-brand-grow");
-			navLogo.classList.remove("navbar-brand-shrink");
-		} else {
-			navContainer.classList.add("navShrink");
-			navParent.classList.add("navParentShrink");
-			// navLogo.classList.remove("navbar-brand-grow");
-			navLogo.classList.add("navbar-brand-shrink");
-		}
-	};
-	handleScroll();
-	document.addEventListener("scroll", handleScroll);
-});
+// 	const handleScroll = () => {
+// 		if (!navContainer) return;
+// 		if (window.scrollY === 0) {
+// 			navContainer.classList.remove("navShrink");
+// 			navParent.classList.remove("navParentShrink");
+// 			// navLogo.classList.add("navbar-brand-grow");
+// 			navLogo.classList.remove("navbar-brand-shrink");
+// 		} else {
+// 			navContainer.classList.add("navShrink");
+// 			navParent.classList.add("navParentShrink");
+// 			// navLogo.classList.remove("navbar-brand-grow");
+// 			navLogo.classList.add("navbar-brand-shrink");
+// 		}
+// 	};
+// 	handleScroll();
+// 	document.addEventListener("scroll", handleScroll);
+// });
